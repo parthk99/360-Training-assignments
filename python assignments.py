5ed5895220d3aa781507f102fad0126c20729841
parthk99 <parthkayastha74@gmail.com>
2022-09-22 21:17:14 -0400

    added python file to repo

---
+++ python assignments.py
@@ -0,0 +1,38 @@
+#Exercise 1.1
+a, b, c = 1, 2.01, "String"
+print(a,b,c)
+
+#Exercise 1.2
+x = 1 + 2j
+print(int(x))
+print(x)
+
+#Exercise 1.3
+x = 10
+y =20 
+print(x,y)
+x , y = y, x 
+print(x,y)
+
+#Exercise 1.4
+user = input("What is your name? ")
+print("hello", user)
+
+#Exercise 1.5
+num1 = input("Enter any number between 1-10 ")
+num2 = input("Enter another number between 1-10 ")
+z = int(num1) + int(num2)
+sum = int(z + 30)
+print(sum)
+
+#Exercise 1.6
+x = "Hello"
+print(type(x))
+
+#Exercise 1.7
+#uppercamelcase = MyCar
+#lowercamelcase = myCar
+#uppercase = MYCAR  
+#snakecase = my_car
+
+#Exercise 1.8
